7a4d9753121d42334faf235e07556b32f9728b9d
GollinoGui <131721946+GollinoGui@users.noreply.github.com>
2025-02-24 20:22:49 -0300

    arma começo

--- src/jogo/Arma.java
+++ src/jogo/Arma.java
@@ -2,4 +2,23 @@ package jogo;
 
 public class Arma extends Item{
 	private double dano_arma;
+	private String tipo_arma; //machado, espada, lança
+
+
+	public double getdano_arma(){
+		return dano_arma;
+	}
+
+	public double setdano_arma(double dano_arma){
+		this.dano_arma = dano_arma;
+	}
+
+	//metodos
+
+	public Arma(){
+		this.dano_arma = (4)
+	}
+
+
+
 }
